c8c5eddee09ce3845d8b7a9035d1f1a7d01eff8b
Gene Francis Stephen V. Brillo <stephbrillo8@gmail.com>
2025-08-13 18:23:44 +0800

    Add files via upload

---
+++ Justafunctionpractice.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void sklubdub(char skrib[]) {
+
+printf("What is up my guy\n");
+printf("My name is Genecis\n");
+printf("Okay then. So you are %s\n", skrib);
+printf("ya idk what me doing rn...\n");
+}
+
+int main() {
+char ass[25] = "Nami";
+float skib = 6.5;
+double lub = 6.78;
+
+sklubdub(ass);
+sklubdub(ass);
+sklubdub(ass);
+
+return 0;
+}
